dd563513816b0a95dbbadcc42959fb1bf645140f
cuong <cuong.dz.1806@gmail.com>
2022-03-13 11:20:58 +0700

    Host initial to origin

--- app/src/main/java/com/vtlallklmc/autobook_btl/MainActivity.java
+++ app/src/main/java/com/vtlallklmc/autobook_btl/MainActivity.java
@@ -10,13 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //la
-        //Khai
-        //manh lee
     }
-    //PCL
-    //hihi
-    //Hackẻ day
-    //Le Manh Cuong day
-    //hú hú
 }
