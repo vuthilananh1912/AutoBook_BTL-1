13b8ee5a2b2f9b3c2d3c078935d51df7e9ef72df
cuong <cuong.dz.1806@gmail.com>
2022-04-05 11:56:35 +0700

    confirm booking dialog

--- app/src/main/java/com/vtlallklmc/autobook_btl/BookingActivity.java
+++ app/src/main/java/com/vtlallklmc/autobook_btl/BookingActivity.java
@@ -2,13 +2,86 @@ package com.vtlallklmc.autobook_btl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class BookingActivity extends AppCompatActivity {
+    Button btnDate,btnTime, btnSave, btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+
+        //ánh xạ
+        btnDate = findViewById(R.id.btnDate);
+        btnTime = findViewById(R.id.btnTime);
+        btnSave = findViewById(R.id.btnSave);
+        btnCancel = findViewById(R.id.btnCancel);
+
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePicker();
+            }
+        });
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timePicker();
+            }
+        });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder confirm = new AlertDialog.Builder(BookingActivity.this);
+                confirm.setTitle("Xác nhận đặt lịch");
+                confirm.setIcon(R.drawable.round_loyalty_24);
+                confirm.setMessage("Bạn có chắc chắn đặt lịch?");
+            }
+        });
+    }
+    public void datePicker(){
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DATE);
+        int month = calendar.get(Calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int nam, int thang, int ngay) {
+                calendar.set(nam,thang,ngay);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                Toast.makeText(BookingActivity.this, simpleDateFormat.format(calendar.getTime()), Toast.LENGTH_SHORT).show();
+            }
+        },year,month,day);
+        datePickerDialog.show();
+    }
+    public void timePicker(){
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int gio, int phut) {
+                calendar.set(0,0,0,gio,phut);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+                Toast.makeText(BookingActivity.this, simpleDateFormat.format(calendar.getTime()), Toast.LENGTH_SHORT).show();
+            }
+        },hour,minute,true);
+        timePickerDialog.show();
     }
 }
